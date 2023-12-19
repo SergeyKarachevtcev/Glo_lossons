59e806a45edf465680a07a5e5930b52b9f1b58b7
SergeyKarachevtcev <estel141081@gmail.com>
2023-12-19 11:29:45 +0300

    переопределил констатнты , исправил возведение в степень

--- js/hardLesson.js
+++ js/hardLesson.js
@@ -1,16 +1,15 @@
-let numers = 266219;
+const numers = 266219;
 
-let numArray = numers.toString().split("").map(Number);
-
+const numArray = numers.toString().split("").map(Number);
 
 let numArraySumm = 1;
 
 for (let i = 0; i < numArray.length; i++) {
 	numArraySumm *= numArray[i];
 }
+console.log(numArraySumm);
 
-
-let result = numArraySumm * numArraySumm * numArraySumm;
+const result = numArraySumm ** 3;
 console.log(result);
 
 console.log(String(result).slice(0, 2));
