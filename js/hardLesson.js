1a903eba354f5c3b4d0d3d4915b0f4982f98f4b8
SergeyKarachevtcev <estel141081@gmail.com>
2023-12-20 14:30:46 +0300

    исправил массив , обернул данные в ковычки

--- js/hardLesson.js
+++ js/hardLesson.js
@@ -1,14 +1,15 @@
 "use strict";
 let lang = prompt("Какой язык у сайта ?", "пример: ru или en");
-const ruLang = [понедельник, вторник, среда, черверг, пятница, суббота, воскресенье];
-const enLang = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday];
+const ruLang = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+const enLang = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const wrongLang = "что то пошло не так";
 
 if (lang === "ru") {
 	console.log(ruLang);
-} if (lang === "en") {
+}
+if (lang === "en") {
 	console.log(enLang);
-}else{
+} else {
 	console.log(wrongLang);
 }
 
@@ -19,8 +20,8 @@ switch (lang) {
 	case "en":
 		console.log(enLang);
 		break;
-	default :
-	console.log(wrongLang);
+	default:
+		console.log(wrongLang);
 }
 
 let langIndex = lang === "ru" ? 0 : lang === "en" ? 1 : -1;
@@ -28,7 +29,7 @@ let daysOfWeek = langIndex === 0 ? ruLang : langIndex === 1 ? enLang : wrongLang
 
 console.log(daysOfWeek);
 
-const namePerson = prompt("Как ваше имя?" , "");
+const namePerson = prompt("Как ваше имя?", "");
 let role = namePerson === "Артем" ? "директор" : namePerson === "Александр" ? "преподаватель" : "студент";
 
 console.log(role);
