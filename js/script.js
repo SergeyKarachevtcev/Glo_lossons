f904d7c3dae984ea18b97c8750aad66923a67250
SergeyKarachevtcev <estel141081@gmail.com>
2024-01-01 13:02:08 +0300

    вроде готово , пока не так сташно как думал

--- js/script.js
+++ js/script.js
@@ -1,100 +1,97 @@
 "use strict";
 
-let title;
-let screens;
-let screenPrice;
-const rollback = 75;
-let adaptive;
-let additionalServices;
-let additionalServicesPrice;
-let additionalServices02;
-let additionalServicesPrice02;
+/* объект */
 
-let fullPrice;
-let servicePercentPrice;
-let allServicePrices;
+const appData = {
+	title: "",
+	screens: "",
+	screenPrice: 0,
+	rollback: 75,
+	adaptive: true,
+	additionalServices: "",
+	additionalServices02: "true",
+	fullPrice: 0,
+	servicePercentPrice: 0,
+	allServicePrices: 0,
 
-/* функции */
-
-/* функция проверки занчения  */
-const isNumber = function (num) {
-	return !isNaN(parseFloat(num)) && isFinite(num);
-};
-
-/* функция со всеми вопросами */
-const asking = function () {
-	title = prompt("Как называется ваш проект?", "");
-	screens = prompt("Какие типы экранов нужно разработать?", "Пример : Простые, Сложные, Интерактивные");
-	do {
-		screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
-	} while (!isNumber(screenPrice));
-	adaptive = confirm("Нужен ли адаптив на сайте?");
-};
-
-/* общая цена  доп услуги */
-const getAllServicePrices = function () {
-	let sum = 0;
-	let price = 0;
-	for (let i = 0; i < 2; i++) {
-		if (i === 0) {
-			additionalServices = prompt("Какой дополнительный тип услуги нужен?");
-		} else if (i === 1) {
-			additionalServices02 = prompt("Какой дополнительный тип услуги нужен?");
-		}
+	/* метод проверки занчения  */
+	isNumber: function (num) {
+		return !isNaN(parseFloat(num)) && isFinite(num);
+	},
+	/* метод со всеми вопросами */
+	asking: function () {
+		appData.title = prompt("Как называется ваш проект?", "");
+		appData.screens = prompt("Какие типы экранов нужно разработать?", "Пример : Простые, Сложные, Интерактивные");
 		do {
-			price = +prompt("Сколько это будет стоить?", "пример: 12000");
-		} while (!isNumber(price));
-		sum += price;
-	}
-	return sum;
+			appData.screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
+		} while (!appData.isNumber(appData.screenPrice));
+		appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+	},
+
+	/* общая цена  доп услуги */
+	getAllServicePrices: function () {
+		let sum = 0;
+		let price = 0;
+		for (let i = 0; i < 2; i++) {
+			if (i === 0) {
+				appData.additionalServices = prompt("Какой дополнительный тип услуги нужен?");
+			} else if (i === 1) {
+				appData.additionalServices02 = prompt("Какой дополнительный тип услуги нужен?");
+			}
+			do {
+				price = +prompt("Сколько это будет стоить?", "пример: 12000");
+			} while (!appData.isNumber(price));
+			sum += price;
+		}
+		return sum;
+	},
+
+	/* общий тотал (цена работы + доп услуги)  */
+	getFullPrice: function () {
+		return appData.screenPrice + appData.allServicePrices;
+	},
+
+	/* формат заголовка */
+	getTitle: function () {
+		const trimmedTitle = appData.title.trim();
+		let formattedTitle = trimmedTitle.charAt(0).toUpperCase() + trimmedTitle.slice(1).toLowerCase();
+		return formattedTitle;
+	},
+
+	/* итоговую стоимость за вычетом процента отката */
+	getServicePercentPrices: function () {
+		return Math.ceil(appData.fullPrice - appData.rollback);
+	},
+
+	/* размер скидки */
+	getRollbackMessage: function (price) {
+		if (price >= 0 && price <= 15000) {
+			return "Скидка не предусмотрена";
+		}
+		if (price > 15000 && price <= 30000) {
+			return "Даем скидку в 5%";
+		}
+		if (price < 0) {
+			return "Что-то пошло не так";
+		} else {
+			return "Даем скидку в 10%";
+		}
+	},
+	start: function () {
+		appData.asking(); /* вызов метода с вопросами */
+		appData.allServicePrices = appData.getAllServicePrices(); /* вызов метода (сложение доп услуг ) */
+		appData.fullPrice = appData.getFullPrice(); /* цена за работу + доп услуги */
+		appData.title = appData.getTitle(); /* форматирование названия проекта */
+		appData.servicePercentPrice = appData.getServicePercentPrices(); /* фулл прайс - откат */
+		this.loger();
+	},
+	loger: function () {
+		console.log(appData.getTitle());
+		console.log("allServicePrices " + appData.allServicePrices); /* стоимость доп услуг */
+		console.log(appData.getRollbackMessage(appData.fullPrice)); /* цена за работу + доп услуги */
+		console.log("Стоимость верстки экранов " + appData.screenPrice + " рублей." + " Стоимость разработки сайта " + appData.fullPrice + " рублей.");
+	},
 };
 
-/* общий тотал (цена работы + доп услуги)  */
-function getFullPrice() {
-	return screenPrice + allServicePrices;
-}
-
-/* формат заголовка */
-const getTitle = function () {
-	const trimmedTitle = title.trim();
-	let formattedTitle = trimmedTitle.charAt(0).toUpperCase() + trimmedTitle.slice(1).toLowerCase();
-	return formattedTitle;
-};
-
-/* итоговую стоимость за вычетом процента отката */
-const getServicePercentPrices = function () {
-	return Math.ceil(fullPrice - rollback);
-};
-
-const showTypeOf = function (variable) {
-	console.log(variable, typeof variable);
-};
-
-/* размер скидки */
-function getRollbackMessage(price) {
-	if (price >= 0 && price <= 15000) {
-		return "Скидка не предусмотрена";
-	}
-	if (price > 15000 && price <= 30000) {
-		return "Даем скидку в 5%";
-	}
-	if (price < 0) {
-		return "Что-то пошло не так";
-	} else {
-		return "Даем скидку в 10%";
-	}
-}
-
-/* вызов функций */
-asking(); /* вызов функции с вопросами */
-allServicePrices = getAllServicePrices(); /* вызов функции (сложение доп услуг ) */
-fullPrice = getFullPrice(); /* цена за работу + доп услуги */
-title = getTitle(); /* форматирование названия проекта */
-servicePercentPrice = getServicePercentPrices(); /* фулл прайс - откат */
-
-showTypeOf(title); /* вызов функции проверки типа данных */ /* вывожу сообщение о скидке , вызываю функцию*/
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-console.log(screens.length);
-console.log(getRollbackMessage(fullPrice));
-console.log("Стоимость верстки экранов " + screenPrice + " рублей." + " Стоимость разработки сайта " + fullPrice + " рублей.");
+/* вызов метода */
+appData.start();
