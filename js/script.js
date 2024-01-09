4b71cb8f62a8609b99e1e482a1c643c914f4a46a
SergeyKarachevtcev <estel141081@gmail.com>
2024-01-09 15:52:51 +0300

    внес необходимые правки . вывел в консаль в отдельную переменную каждую кнопку

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const titleElement = document.getElementsByTagName("h1")[0];
 console.log(titleElement);
 
 const handlerBtn = document.getElementsByClassName("handler_btn");
-console.log(handlerBtn[0]);
-console.log(handlerBtn[1]);
+const printHandlerBtn = handlerBtn[0]; // Access the first button
+const cancellHandlerBtn = handlerBtn[1]; // Access the second button
+
+console.log(printHandlerBtn);
+console.log(cancellHandlerBtn);
 
 const screenBtn = document.querySelector(".screen-btn");
 console.log(screenBtn);
@@ -21,11 +24,15 @@ const rangeValue = document.querySelector(".rollback .range-value");
 console.log(rangeValue);
 
 const totalInput = document.getElementsByClassName("total-input");
-const totalInputElements = [];
-for (let i = 0; i < totalInput.length; i++) {
-	totalInputElements.push(totalInput[i]);
-}
-console.log(totalInputElements);
+const mainTotalCount = totalInput[0];
+const totalCountTotalInput = totalInput[1];
+const totalCountOther = totalInput[2];
+const totalFullCount = totalInput[3];
+const totalCountRollback = totalInput[4];
+
+console.log(mainTotalCount, totalCountTotalInput, totalCountOther, totalFullCount, totalCountRollback);
+
+
 
 let screen = document.querySelectorAll(".screen");
 console.log(screen);
