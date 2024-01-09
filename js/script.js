68ee79891b886ec765e1bee5d11260a56989bf86
SergeyKarachevtcev <estel141081@gmail.com>
2024-01-09 12:51:45 +0300

    новая домашка , вроде все путем )

--- js/script.js
+++ js/script.js
@@ -1,120 +1,31 @@
-"use strict";
+const titleElement = document.getElementsByTagName("h1")[0];
+console.log(titleElement);
 
-/* объект */
+const handlerBtn = document.getElementsByClassName("handler_btn");
+console.log(handlerBtn[0]);
+console.log(handlerBtn[1]);
 
-const appData = {
-	title: "",
-	screens: [],
-	screenPrice: 0,
-	rollback: 75,
-	adaptive: true,
-	additionalServices: {},
-	fullPrice: 0,
-	servicePercentPrice: 0,
-	allServicePrices: 0,
+const screenBtn = document.querySelector(".screen-btn");
+console.log(screenBtn);
 
-	/* проверка на строку */
-	isString: function (value) {
-		return typeof value === "string";
-	},
-	/* метод проверки занчения  */
-	isNumber: function (num) {
-		return !isNaN(parseFloat(num)) && isFinite(num);
-	},
-	/* метод со всеми вопросами */
-	asking: function () {
-		do {
-			appData.title = prompt("Как называется ваш проект?", "");
-		} while (!appData.isString(appData.title) || !isNaN(parseFloat(appData.title)));
+const otherItemsNum = document.querySelectorAll(".other-items.number");
+console.log(otherItemsNum);
 
-		for (let i = 0; i < 2; i++) {
-			let price = 0;
-			let name = "";
+const otherItemsProcent = document.querySelectorAll(".other-items.percent");
+console.log(otherItemsProcent);
 
-			do {
-				name = prompt("Какие типы экранов нужно разработать?", "Пример : Простые, Сложные, Интерактивные");
-			} while (!appData.isString(name) || !isNaN(parseFloat(name)));
+const rangeInput = document.querySelector('.rollback input[type="range"]');
+console.log(rangeInput);
 
-			do {
-				price = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
-			} while (!appData.isNumber(price) || isNaN(parseFloat(price)));
-			appData.screens.push({ id: i, name: name, price: price });
-		}
+const rangeValue = document.querySelector(".rollback .range-value");
+console.log(rangeValue);
 
-		for (let i = 0; i < 2; i++) {
-			let price = 0;
-			let name = "";
+const totalInput = document.getElementsByClassName("total-input");
+const totalInputElements = [];
+for (let i = 0; i < totalInput.length; i++) {
+	totalInputElements.push(totalInput[i]);
+}
+console.log(totalInputElements);
 
-			do {
-				name = prompt("Какой дополнительный тип услуги нужен?", "");
-			} while (!appData.isString(name) || !isNaN(parseFloat(name)));
-
-			do {
-				price = +prompt("Сколько это будет стоить?", "пример: 12000");
-			} while (!appData.isNumber(price));
-			appData.additionalServices[name] = +price;
-		}
-
-		appData.adaptive = confirm("Нужен ли адаптив на сайте?");
-	},
-
-	addPrices: function () {
-		for (let screen of appData.screens) {
-			appData.screenPrice += +screen.price;
-		}
-		/* общая цена  доп услуги */
-		for (let key in appData.additionalServices) {
-			appData.allServicePrices += appData.additionalServices[key];
-		}
-	},
-
-	/* общий тотал (цена работы + доп услуги)  */
-	getFullPrice: function () {
-		appData.fullPrice = appData.screenPrice + appData.allServicePrices;
-	},
-
-	/* формат заголовка */
-	getTitle: function () {
-		const trimmedTitle = appData.title.trim();
-		appData.title = trimmedTitle.charAt(0).toUpperCase() + trimmedTitle.slice(1).toLowerCase();
-		return;
-	},
-
-	/* итоговую стоимость за вычетом процента отката */
-	getServicePercentPrices: function () {
-		appData.servicePercentPrice = Math.ceil(appData.fullPrice - appData.rollback);
-	},
-
-	/* размер скидки */
-	getRollbackMessage: function (price) {
-		if (price >= 0 && price <= 15000) {
-			return "Скидка не предусмотрена";
-		}
-		if (price > 15000 && price <= 30000) {
-			return "Даем скидку в 5%";
-		}
-		if (price < 0) {
-			return "Что-то пошло не так";
-		} else {
-			return "Даем скидку в 10%";
-		}
-	},
-	start: function () {
-		appData.asking(); /* вызов метода с вопросами */
-		appData.addPrices();
-
-		appData.getFullPrice(); /* цена за работу + доп услуги */
-		appData.getTitle(); /* форматирование названия проекта */
-		appData.getServicePercentPrices(); /* фулл прайс - откат */
-
-		appData.logger();
-	},
-	logger: function () {
-		for (let key in appData) {
-			console.log(key + ": " + appData[key]);
-		}
-	},
-};
-
-/* вызов метода */
-appData.start();
+let screen = document.querySelectorAll(".screen");
+console.log(screen);
