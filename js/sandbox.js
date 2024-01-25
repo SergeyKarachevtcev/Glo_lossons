84828ab08a62f1b5095ca1f8753c1ce73f3e53bd
SergeyKarachevtcev <estel141081@gmail.com>
2024-01-25 16:17:12 +0300

    поправил домашку

--- js/sandbox.js
+++ js/sandbox.js
@@ -8,22 +8,24 @@ function DomElement(selector, height, width, bg, fontSize) {
 	this.createElement = function () {
 		let element;
 		if (this.selector.startsWith(".")) {
+			const className = this.selector.slice(1);
 			element = document.createElement("div");
+			element.classList.add(className);
 		} else if (this.selector.startsWith("#")) {
 			element = document.createElement("p");
+			element.id = this.selector.slice(1);
 		}
 		element.style.height = this.height;
 		element.style.width = this.width;
 		element.style.background = this.bg;
 		element.style.fontSize = this.fontSize;
 		element.textContent = "Любой текст";
-		return element;
+		document.body.appendChild(element);
 	};
 }
 
-const newElem = new DomElement(".block", "100px", "200px", "blue", "25px");
+const newElem = new DomElement("#block", "100px", "200px", "blue", "25px");
 
-const container = document.getElementById("container");
-container.appendChild(newElem.createElement());
+newElem.createElement();
 
 console.log(newElem);
